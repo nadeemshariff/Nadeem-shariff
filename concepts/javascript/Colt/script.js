f70db51c8b4a9aa600442e16be54ac6a2497c74b
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-08-18 21:23:34 +0530

    Creating Nav bar and banner

---
+++ concepts/javascript/Colt/script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+// const banner = document.getElementById('banner');
+//         const closeBanner = () => banner.style.display = 'none';
+
+//         // when subscribe button is clicked send an email to the user.
+
+//         const subscribe = document.getElementById('subscribe');
+//         const emailInput = document.getElementById('email');
+
+//         subscribe.addEventListener('click', () => {
+//             const email = emailInput.value;
+//             console.log(email);
+//         });
+
+// // Banner close function.
+
+// // const banner = document.getElementById('banner');
+// //         const closeBanner = () => banner.style.display = 'none';
+// // // function closeBanner() {
+// // //     document.getElementById('banner').style.display = 'none';
+// // // }
