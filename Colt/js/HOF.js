168e4944e3a62f8f9726280fe724e75f4d9663e3
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-08-22 00:41:56 +0530

    Higher order Array Loops.

---
+++ Colt/js/HOF.js
@@ -0,0 +1,121 @@
+// HOF
+
+// ["","",""]
+//[{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+}
+
+
+for (const num in arr) {
+    if (Object.prototype.hasOwnProperty.call(arr, num)) {
+        const element = arr[num];
+        console.log(num);
+    }
+}
+
+// Maps
+// const map = new Map();
+
+// map.set("name", "Rohini");
+// map.set("name", "Mansha");
+// map.set("name", "Anusha");
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value)
+// }
+
+// const myObj = {
+//     'game1': 'NFS',
+//     'game2': 'GTA'
+// }
+// for (const [key,value] of myObj) {
+//     console.log(key, "-", value);
+// }
+
+// console.log(map.get("name"));
+
+// console.log(map.size);
+
+// map.delete("name");
+
+// console.log(map.has("name"));
+
+// map.clear();
+
+// console.log(map.size);
+
+// map.set("name", "Rohini");
+
+// console.log(map);
+
+const obj = {
+    name: "Rohini",
+    age: 22,
+    category: "ebony",
+
+    // display: function() {
+    //     console.log(`Name: ${this.name}`);
+    //     console.log(`Age: ${this.age}`);
+    //     console.log(`Category: ${this.category}`);
+    // }
+}
+
+// obj.display();
+for (const key in obj) {
+    console.log(`${key} shortcut is for ${obj[key]}`);
+}
+
+const map = new Map();
+map.set("name", "Rohini");
+map.set("age", 25);
+map.set("category", "ebony");
+map.set("figure", "petite");
+
+// for (const key in map){
+//     console.log(key);
+// }
+
+const coding = ["A", "B","C"];
+
+coding.forEach((value, index) => {
+    console.log(value, index);
+});
+
+function printMe(item){
+    console.log(item);
+}
+
+coding.forEach(printMe);
+
+coding.forEach((item, index, array) => {
+    console.log(item, index, array);
+})
+
+const newCoding = [
+    {
+        langName: "javasript",
+        langFileName: "js"
+    },
+    {
+        langName: "java",
+        langFileName: "java"
+    },
+    {
+        langName: "python",
+        langFileName: "py"
+    },
+    {
+        langName: "c##",
+        langFileName: "c++"
+    }
+]
+
+newCoding.forEach((item) => {
+    console.log(item.langName);
+    console.log(item.langName, item.langFileName);
+});
--- Colt/js/controlflow.js
+++ Colt/js/controlflow.js
@@ -146,3 +146,63 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+//Loops
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(i);
+}
+// console.log(element);
+
+for (let i = 0; i <=10; i++) {
+    console.log(`Outerloop value: ${i}`)
+    // const element = array[i];
+    for (let j = 0; j <= 10; j++) {
+        // const element = array[j];
+        console.log(i + "*" + j + " = " + i*j)
+        
+    }
+}
+
+for (let i = 1; i <= 20 ; i++) {
+
+    if (i == 5) {
+        console.log(`Detected ${i}`);
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+    
+}
+
+
+// While
+
+let arr = ['a','b','c']
+
+let newArr = 0;
+
+while (newArr < arr.length) {
+    console.log(`value is ${arr[newArr]}`);
+    newArr = newArr + 1
+}
+
+
+// For in
+
+const person = {
+    name: "rohini",
+    age: 24
+}
+
+for (const key in person) {
+    console.log(key);
+    console.log(person[key]);
+}
+
+// For of
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const element of arr1) {
+    console.log(element);
+}
