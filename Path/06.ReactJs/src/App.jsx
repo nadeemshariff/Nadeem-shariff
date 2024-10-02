89bb224339083129cd40fa8aa1b0c85a064d95d4
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-10-02 20:23:27 +0530

    NumberPicker

---
+++ Path/06.ReactJs/src/App.jsx
@@ -0,0 +1,16 @@
+// import './App.css';
+import NumberPicker from './component/NumberPicker'
+import StandardReactLayout from './component/StandardReactLayout'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NumberPicker />
+      <StandardReactLayout/>
+    </>
+  )
+}
+
+export default App
---
+++ Path/06.ReactJs/src/component/NumberPicker.jsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+
+const NumberPicker = () => {
+    function getNum() {
+        return Math.floor(Math.random() * 10) + 1;
+      }
+      const num = getNum();
+      let msg;
+    
+      if (num === 7) {
+        msg = <div>
+          <h2>Congrats You Win</h2>
+          <img src="https://i.giphy.com/media/nXx0jZrbnbRxS/giphy.webp" />
+        </div>;
+      } else { 
+        msg = 'BOIOIOI';
+      }
+  return (
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="bg-white rounded-lg shadow-md p-8 max-w-md w-full">
+          <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">Your number is: <span className="text-indigo-600">{num}</span></h1>
+          <div className="text-center">
+            {msg}
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default NumberPicker
