d119cc68252350587cbccebc32e993b5ace39ddb
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-10-03 02:21:27 +0530

    Slot Machine

---
+++ Path/06.ReactJs/src/component/slotMachine/Machine.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+const Machine = () => {
+    // const {s1,s2,s3} = this.props;
+    // const winner = ((s1 ===s2) && (s2 === s3) );
+
+  return (
+    // <div>
+    //     <p> {s1} {s2} {s3} </p>
+    //     <p>{winner ? 'winner!' : 'Loser!'}</p>
+    // </div>
+    <div className="">
+        <h1>Machine</h1>
+    </div>
+  )
+}
+
+export default Machine
